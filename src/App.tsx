f800226777b6e27f05cbad55e8f4742a7262802a
“Ihan0316” <aijnjj12@gmail.com>
2025-03-27 23:49:51 +0900

    25/3/27 firebase 연결전 준비작업1

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Login from './router/login.tsx';
 import CreateAccount from './router/create-account.tsx';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import { useEffect, useState } from 'react';
+import LoadingScreen from './components/loading-screen.tsx';
 
 const router = createBrowserRouter([
   {
@@ -48,10 +50,18 @@ const GlobalStyles = createGlobalStyle`
 `;
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const init = async () => {
+    // wait for firebase
+    setLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
     </>
   );
 }
